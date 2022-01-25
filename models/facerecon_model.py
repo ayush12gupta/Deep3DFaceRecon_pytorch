ffaf97bf9fc4e5c63cb46f7e4f667093aa5c160c
ayush12gupta <gupta101a12@gmail.com>
2022-01-25 23:22:01 +0530

    fix

--- models/facerecon_model.py
+++ models/facerecon_model.py
@@ -50,12 +50,12 @@ class FaceReconModel(BaseModel):
             # loss weights
             parser.add_argument('--w_feat', type=float, default=0.2, help='weight for feat loss')
             parser.add_argument('--w_color', type=float, default=2.92, help='weight for loss loss')
-            parser.add_argument('--w_reg', type=float, default=3.0e-4, help='weight for reg loss')
-            parser.add_argument('--w_id', type=float, default=1.0, help='weight for id_reg loss')
-            parser.add_argument('--w_exp', type=float, default=0.8, help='weight for exp_reg loss')
+            parser.add_argument('--w_reg', type=float, default=9.0e-4, help='weight for reg loss')
+            parser.add_argument('--w_id', type=float, default=4.0, help='weight for id_reg loss')
+            parser.add_argument('--w_exp', type=float, default=2, help='weight for exp_reg loss')
             parser.add_argument('--w_tex', type=float, default=1.7e-2, help='weight for tex_reg loss')
             parser.add_argument('--w_gamma', type=float, default=10.0, help='weight for gamma loss')
-            parser.add_argument('--w_lm', type=float, default=1.6e-3, help='weight for lm loss')
+            parser.add_argument('--w_lm', type=float, default=3.2e-3, help='weight for lm loss')
             parser.add_argument('--w_reflc', type=float, default=0.01, help='weight for reflc loss')
 
 
